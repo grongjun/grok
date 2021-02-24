4188d970402229827a9854a1073ac239830efe68
Rongjun Gao <rongjun.gao@foxmail.com>
2021-02-24 19:01:11 +0800

    examples of lattice usage

---
+++ examples/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+import "github.com/grongjun/grok"
+
+func main() {
+
+	// Lattice definition
+
+	// case 1
+	pl := grok.NewLattice(`{ "name": "Purpose", "edges": { "Sharing": []} }`)
+	fmt.Println(*pl)
+
+	// case 2:
+
+
+	// Policy definition
+
+	// case 1
+
+
+	// case 2
+
+
+
+	// ApplyOn
+
+	
+
+}
+
